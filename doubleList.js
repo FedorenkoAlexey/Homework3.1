342c617b5ec8b75acfb8243878f7fd0b58ba0e76
Alexey Fedorenko <messer.ua@gmail.com>
2019-11-04 20:55:31 +0200

    fix swap

--- doubleList.js
+++ doubleList.js
@@ -1,100 +1,103 @@
 class ListElement {
-    constructor(value) {
-        this.value = value;
-        this.next = null;
-        this.prev = null;
-    }
-    setValue(value) {
-        this.value = value;
-    }
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+    this.prev = null;
+  }
+  setValue(value) {
+    this.value = value;
+  }
 
-    getNext() {
-        return this.next;
-    }
+  getNext() {
+    return this.next;
+  }
 
-    setNext(element) {
-        this.next = element;
-        //      if (element != null) {
-        //          element.prev = this;
-        //      }
+  setNext(element) {
+    this.next = element;
+    if (element != null) {
+      element.prev = this;
     }
+  }
 
-    getPrev() {
-        return this.prev;
+  getPrev() {
+    return this.prev;
+  }
+
+  setPrev(element) {
+    this.prev = element;
+    if (element != null) {
+      element.next = this;
     }
+  }
 
-    setPrev(element) {
+  cut() {
+    let p = this.getPrev();
+    let n = this.getNext();
+    this.next = null;
+    this.prev = null;
+
+    if (p != null) {
+      p.setNext(n);
+    }
+    if (n != null) {
+      n.setPrev(p);
+    }
+  }
+
+  swap(element) {
+    let p = this.getPrev();
+    let n = this.getNext();
+    let p2 = element.getPrev();
+    let n2 = element.getNext();
+
+    if (p != element) {
+      if (n == element) {
         this.prev = element;
-        //      if (element != null) {
-        //          element.next = this;
-        //      }
-    }
+        element.next = this;
+        this.setNext(n2);
+        element.setPrev(p);
+      } else {
+        if (n != null) {
+          n.setPrev(element);
+          element.setNext(n);
+        } else {
+          element.next = null;
+        }
 
-    cut() {
-        let p = this.getPrev();
-        let n = this.getNext();
-        this.next = null;
-        this.prev = null;
+        if (n2 != null) {
+          this.setNext(n2);
+          n2.setPrev(this);
+        } else {
+          this.next = null;
+        }
 
         if (p != null) {
-            p.setNext(n);
-        } else if (n != null) {
-            n.setPrev(p);
-        }
-    }
-
-    swap(element) {
-        let p = this.getPrev();
-        let n = this.getNext();
-        let p2 = element.getPrev();
-        let n2 = element.getNext();
-
-        if (p != element) {
-            if (n == element) {
-                this.prev = element;
-                element.next = this;
-            }
-            if (n != null) {
-                n.setPrev(element);
-                element.setNext(n);
-            } else {
-                element.next = null;
-            }
-
-            if (n2 != null) {
-                this.setNext(n2);
-                n2.setPrev(this);
-            } else {
-                this.next = null;
-            }
-
-            if (p != null) {
-                element.setPrev(p);
-                p.setNext(element);
-            } else {
-                element.prev = null;
-            }
-            if (p2 != null) {
-                this.setPrev(p2);
-                p2.setNext(this);
-            } else {
-                this.prev = null;
-            }
+          element.setPrev(p);
+          p.setNext(element);
         } else {
-            this.next = element;
-            element.prev = this;
-            this.setPrev(p2);
-            element.setNext(n);
-
-            if (p2 == null) {
-                this.prev = null;
-            } else if (n == null) {
-                element.next = null;
-            }
+          element.prev = null;
         }
-    }
-}
+        if (p2 != null) {
+          this.setPrev(p2);
+          p2.setNext(this);
+        } else {
+          this.prev = null;
+        }
+      }
+    } else {
+      this.next = element;
+      element.prev = this;
+      this.setPrev(p2);
+      element.setNext(n);
 
+      if (p2 == null) {
+        this.prev = null;
+      } else if (n == null) {
+        element.next = null;
+      }
+    }
+  }
+}
 
 let el1 = new ListElement();
 let el2 = new ListElement();
